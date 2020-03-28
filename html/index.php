7cf4297b7532bd986a4ffd982b93ba91bb8d8e95
jyef <tubudakusan@gmail.com>
2020-03-28 13:46:59 +0900

    create question.php

--- html/index.php
+++ html/index.php
@@ -1,24 +1,48 @@
 <?php
+session_start();
 require('dbconnect.php');
 
-$sjis = file_get_contents('./sistan.csv');
-$utf8 = mb_convert_encoding($sjis, 'UTF-8', 'SJIS-win');
-file_put_contents('./utf8.csv', $utf8);
+if (isset($_POST['action']) && $_POST['action'] == 'importcsv') {
+    $sjis = file_get_contents('./sistan.csv');
+    $utf8 = mb_convert_encoding($sjis, 'UTF-8', 'SJIS-win');
+    file_put_contents('./utf8.csv', $utf8);
+    
+    $f = fopen("./utf8.csv", "r");
+    
+    while($line = fgetcsv($f)){
+        //重複チェック
+        $id = $db->prepare('SELECT count(*) AS cnt FROM mytable WHERE id=?');
+        $id->execute(array($line[0]));
+        $count = $id->fetch();
+        //重複していなかったらインサート
+        if ($count['cnt'] === '0') {
+            $statement = $db->prepare('INSERT INTO mytable SET id=?, enwords=?, jpwords=?');
+            $statement->execute(array($line[0], $line[1], $line[2]));
+        }
+    }
+    fclose($f);
+    $message['import'] = 'インポートが完了しました';
+}
 
-$f = fopen("./utf8.csv", "r");
+if (isset($_GET['action']) && $_GET['action'] == 'inputnum') {
+    if (!empty($_GET['start']) && !empty($_GET['end']) && is_numeric($_GET['start']) && is_numeric($_GET['end'])) {
+        $start = (int) $_GET['start'];
+        $end = (int) $_GET['end'];
+        if ($start > $end) {
+            $error['diff'] = '※不正な範囲です';
+        }
+    } else {
+        $error['num'] = '※数値を入力してください';
+    }
 
-while($line = fgetcsv($f)){
-    //重複チェック
-    $id = $db->prepare('SELECT count(*) AS cnt FROM mytable WHERE id=?');
-    $id->execute(array($line[0]));
-    $count = $id->fetch();
-    //重複していなかったらインサート
-    if ($count['cnt'] === '0') {
-        $statement = $db->prepare('INSERT INTO mytable SET id=?, enwords=?, jpwords=?');
-        $statement->execute(array($line[0], $line[1], $line[2]));
+    if (empty($error)) {
+        unset($_SESSION['join']);
+        $_SESSION['join']['start'] = $start;
+        $_SESSION['join']['end'] = $end;
+        header('Location: question.php');
+        exit();
     }
 }
-fclose($f);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,8 +50,31 @@ fclose($f);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <form action="" method="post">
+        <input type="hidden" name="action" value="importcsv">
+        <button type="submit">csvインポート</button>
+    </form>
+    <?php $message['import'] ? print '<p>'.$message['import'].'</p>' : '' ; ?>
 
+    <form action="" method="get">
+        <p>出題範囲を整数で入力してください</p>
+        <input type="hidden" name="action" value="inputnum">
+        <input type="text" name="start"> 〜 <input type="text" name="end">
+        <input type="submit" value="送信する">
+    </form>
+    <?php $error['num'] ? print '<p class="warn">'.$error['num'].'</p>' : '' ; ?>
+    <?php $error['diff'] ? print '<p class="warn">'.$error['diff'].'</p>' : '' ; ?>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script>
+        $('.words').on('click', function(){
+            var index = $('.words').index(this);
+            var word = $('.words').eq(index);
+            word.toggleClass('one');
+        });
+    </script>
 </body>
 </html>
